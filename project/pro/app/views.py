40fc300f17ca0280d352a2757a2b3253087de2ef
unknown <ameysarpate26@gmail.com>
2020-06-27 17:18:55 +0530

    Everything working but Reapeted code is too much

--- project/pro/app/views.py
+++ project/pro/app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def home(request):
     col=70;
     row=35;
-    return render(request,'home.html',{'rows':range(1,row+1),'cols':range(1,col+1)})
+    return render(request,'home.html',{'rows':range(1,row+1),'cols':range(1,col+1),'nrows':row,'ncols':col})
 
 
 def side(request):
